98b15cbe4f8e2a27aafcc2ac4f11deb5e969cc38
Nditah <nditah@gmail.com>
2018-05-24 16:06:12 +0100

    hosted on Heroku

--- api/index.js
+++ api/index.js
@@ -1,3 +1,31 @@
+var express = require('express');
+var pg = require("pg");
+var app = express();
+ 
+var connectionString = "    postgres://bfxyknabiikqnx:4597af2a1387cf3aee62700919239a43ecba83a1e82738add365007348b7f06c@ec2-54-217-208-52.eu-west-1.compute.amazonaws.com:5432/d8d2grf4h559oa";
+ 
+app.get('/', function (req, res, next) {
+    pg.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT * FROM tbl_request where id = $1', [1],function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
+ 
+app.listen(3000, function () {
+    console.log('Server is running.. on Port 3000');
+});
+
+/*
 //Now we can set up the route handlers in index.js:
 
 var express = require('express');
@@ -14,3 +42,4 @@ router.delete('/api/requests/:id', db.removeRequest);
 
 
 module.exports = router;
+*/
